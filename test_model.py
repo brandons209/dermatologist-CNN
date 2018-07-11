a2e4802def288a888b3dc378da4b489992d5c57f
Brandon Silva <brandons209@aol.com>
2018-07-11 02:06:32 -0400

    allow user to enter path to trained model to use for testing.

--- test_model.py
+++ test_model.py
@@ -7,6 +7,6 @@ def prediction(img_path, model):
     diease_names = ['melanoma', 'nevus', 'seborrheic keratosis']
     return diease_names[np.argmax(model.predict(process_single_image(img_path)))]
 
-model = load_model('saved_models/skin_cancer_full_model.h5')
+model = load_model(sys.arg[2])
 
 print("That lesion is predicted to be: {}".format(prediction(sys.argv[1], model)))
